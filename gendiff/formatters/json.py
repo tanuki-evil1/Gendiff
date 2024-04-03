bffd7c5eeb750e0c239185de5356dafe5d2be8de
tanuki <tanuki.evil@yandex.ru>
2024-04-03 12:35:51 +0300

    update

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,6 +1,5 @@
 import json
 
 
-def json_formatter(diff_list: dict) -> None:
-    with open('result.json', 'w') as file:
-        json.dump(diff_list, file, ensure_ascii=False, indent=4)
+def json_formatter(diff_list: dict):
+    return json.dumps(diff_list)
--- tests/formatters/test_json.py
+++ tests/formatters/test_json.py
@@ -1,9 +1,11 @@
-import os.path
+import json
 from gendiff.formatters.json import json_formatter
 
 
 def test_json_formatter():
-    json_formatter({'follow': ('-', False), 'host': 'hexlet.io', 'proxy': ('-', '123.234.53.22'),
-                    'timeout': ('-+', (50, 20)), 'verbose': ('+', True)})
-    assert os.path.isfile('result.json')
-    os.remove("result.json")
+    result = json_formatter({'follow': ('-', False), 'host': 'hexlet.io', 'proxy': ('-', '123.234.53.22'),
+                             'timeout': ('-+', (50, 20)), 'verbose': ('+', True)})
+    expected = {'follow': ['-', False], 'host': 'hexlet.io', 'proxy': ['-', '123.234.53.22'],
+                'timeout': ['-+', [50, 20]], 'verbose': ['+', True]}
+
+    assert json.loads(result) == expected
