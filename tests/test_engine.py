a32d21978cacef558ea6c4b28b7dbe782154fef9
tanuki <tanuki.evil@yandex.ru>
2024-04-09 16:12:58 +0300

    fix 4

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -6,27 +6,21 @@ from gendiff.engine import generate_diff
 FIXTURES_PATH = Path('tests/fixtures')
 
 
-# Убрать дубликат Fixture_path и тесты с yaml
 @pytest.mark.parametrize("path1, path2, res_path", [('file1.json',
-                                                     f'file2.json',
-                                                     f'formatters/file_stylish_1_to_2.txt'),
-                                                    (f'file2.json',
-                                                     f'file1.json',
-                                                     f'formatters/file_stylish_2_to_1.txt'),
-                                                    (f'file1.yml',
-                                                     f'file2.yaml',
-                                                     f'formatters/file_stylish_1_to_2.txt'),
-                                                    (f'file2.yaml',
-                                                     f'file1.yml',
-                                                     f'formatters/file_stylish_2_to_1.txt'),
-                                                    (f'file3.yaml',
-                                                     f'file4.json',
-                                                     f'formatters/file_stylish_3_to_4.txt'),
+                                                     'file2.json',
+                                                     'formatters/file_stylish_1_to_2.txt'),
+                                                    ('file2.json',
+                                                     'file1.json',
+                                                     'formatters/file_stylish_2_to_1.txt'),
+                                                    ('file3.yaml',
+                                                     'file4.json',
+                                                     'formatters/file_stylish_3_to_4.txt'),
                                                     ])
-def test_generate_diff(path1, path2, res_path, form='stylish'):
+def test_generate_diff(path1, path2, res_path):
     with open(FIXTURES_PATH / res_path) as file:
         expected = file.read()
-    path1 = f'{FIXTURES_PATH}/{path1}'
-    path2 = f'{FIXTURES_PATH}/{path2}'
-    result = generate_diff(path1, path2, form)
+
+    path1 = FIXTURES_PATH / path1
+    path2 = FIXTURES_PATH / path2
+    result = generate_diff(str(path1), str(path2))
     assert result == expected
--- tests/test_parse_files.py
+++ tests/test_parse_files.py
@@ -1,14 +1,14 @@
+import pytest
 from pathlib import Path
+
 from gendiff.parser import get_file_data
 
 FIXTURES_PATH = Path('tests/fixtures')
 
 
-def test_get_file_data():
-    data = get_file_data(FIXTURES_PATH / 'file1.json')
-    expected = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
-    assert data == expected
-
-    data = get_file_data(FIXTURES_PATH / 'file2.json')
-    expected = {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'}
+@pytest.mark.parametrize('path, expected', [
+    ('file1.json', {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}),
+    ('file2.json', {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'})])
+def test_get_file_data(path, expected):
+    data = get_file_data(FIXTURES_PATH / path)
     assert data == expected
