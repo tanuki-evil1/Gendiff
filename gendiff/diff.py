10d7b069cc7a34506d1721b1aee511cedaeaee5d
tanuki <tanuki.evil@yandex.ru>
2024-04-12 17:57:55 +0300

    fix diff

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -9,9 +9,9 @@ def diff(data1: dict, data2: dict) -> Dict[str, Any]:
         elif key not in data2:
             result[key] = 'removed', data1[key]
         elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
-            result[key] = 'nested', diff(data1[key], data2[key])
+            result[key] = diff(data1[key], data2[key])
         elif data1[key] == data2[key]:
-            result[key] = 'no_change', data1[key]
+            result[key] = data1[key]
         elif data1[key] != data2[key]:
             result[key] = 'updated', (data1[key], data2[key])
 
--- tests/test_diff.py
+++ tests/test_diff.py
@@ -2,7 +2,7 @@ from gendiff.diff import diff
 
 
 def test_diff() -> None:
-    expected = {'follow': ('removed', False), 'host': ('no_change', 'hexlet.io'),
+    expected = {'follow': ('removed', False), 'host': 'hexlet.io',
                 'proxy': ('removed', '123.234.53.22'), 'timeout': ('updated', (50, 20)), 'verbose': ('added', True)}
 
     dict1 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
