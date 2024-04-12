5fc1950b884dd61741a0063dfc5b154c36740e1f
tanuki <tanuki.evil@yandex.ru>
2024-04-12 17:51:05 +0300

    refactoring diff and parser

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,28 +1,26 @@
 import json
 import yaml
-import sys
 from pathlib import Path
 from typing import Dict, Any
 
 
+def get_json(file_path: Path) -> Dict[str, Any]:
+    with open(file_path) as file:
+        return json.load(file)
+
+
+def get_yaml(file_path: Path) -> Dict[str, Any]:
+    with open(file_path) as file:
+        return yaml.load(file, Loader=yaml.Loader)
+
+
 def get_file_data(file_path: Path) -> Dict[str, Any]:
     try:
-        with open(file_path) as file:
-            if file_path.name.endswith('json'):
-                return json.load(file)
-            elif file_path.name.endswith(('yaml', 'yml')):
-                return yaml.load(file, Loader=yaml.Loader)
-    # TODO: Как запускать ошибки и отлавливать их в тестах
-    except FileNotFoundError:
-        print('File not found.')
-        sys.exit(1)
-    except json.decoder.JSONDecodeError:
-        print('Incorrect JSON.')
-        sys.exit(1)
-    except yaml.YAMLError:
-        print('Incorrect YAML.')
-        sys.exit(1)
-    except PermissionError:
-        print('Permission denied.')
-        sys.exit(1)
-    # TODO: Надо это все отловить
+        if file_path.name.endswith('json'):
+            return get_json(file_path)
+        elif file_path.name.endswith(('yaml', 'yml')):
+            return get_yaml(file_path)
+        else:
+            raise ValueError('Invalid path to the json or yaml file')
+    except (json.decoder.JSONDecodeError, yaml.YAMLError):
+        raise SyntaxError
