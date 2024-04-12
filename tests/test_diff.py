2e9214beb76a8f14216cc5cf0ef38b5fce6c79d3
tanuki <tanuki.evil@yandex.ru>
2024-04-12 17:51:27 +0300

    add new tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -2,8 +2,9 @@ from gendiff.diff import diff
 
 
 def test_diff() -> None:
-    expected = {'follow': ('removed', False), 'host': 'hexlet.io', 'proxy': ('removed', '123.234.53.22'),
-                'timeout': ('updated', (50, 20)), 'verbose': ('added', True)}
+    expected = {'follow': ('removed', False), 'host': ('no_change', 'hexlet.io'),
+                'proxy': ('removed', '123.234.53.22'), 'timeout': ('updated', (50, 20)), 'verbose': ('added', True)}
+
     dict1 = {'host': 'hexlet.io', 'timeout': 50, 'proxy': '123.234.53.22', 'follow': False}
     dict2 = {'timeout': 20, 'verbose': True, 'host': 'hexlet.io'}
     result = diff(dict1, dict2)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -11,3 +11,9 @@ def test_get_file_data(path1: Path, path2: Path) -> None:
     data1 = get_file_data(FIXTURES_PATH / path1)
     data2 = get_file_data(FIXTURES_PATH / path2)
     assert data1 == data2
+
+
+@pytest.mark.parametrize('path, error', [('', ValueError), ('file0.json', SyntaxError)])
+def test_get_file_data_errors(path: str, error) -> None:
+    with pytest.raises(error):
+        get_file_data(FIXTURES_PATH / path)
